29d1dd891d61761a852774b6f8c99dc98fb66fbd
Nando Machado <nando.carnielmachado@sainsburys.co.uk>
2024-12-06 17:06:20 +0000

    day 1 part 2

--- 1/1.py
+++ 1/1.py
@@ -4,22 +4,37 @@ def calculateDistances (a, b) -> int:
 with open("./input.txt", "r") as f:
     lines = f.readlines()
 
-    # To allocate the correct memory size from the outset
-    listLeft, listRight = [0 for _ in range(len(lines))], [0 for _ in range(len(lines))]
-    
-    for index, item in enumerate(lines):
-        listLeft[index] = int(item.split()[0])
-        listRight[index] = int(item.split()[1])
+# To allocate the correct memory size from the outset
+listLeft, listRight = [0 for _ in range(len(lines))], [0 for _ in range(len(lines))]
 
-    listLeft.sort()
-    listRight.sort()
+for index, item in enumerate(lines):
+    listLeft[index] = int(item.split()[0])
+    listRight[index] = int(item.split()[1])
 
-    total = 0
+# part one
+listLeft.sort()
+listRight.sort()
 
-    for a, b in zip(listLeft, listRight):
-        distance = calculateDistances(a, b)
-        total += distance
-        
+firstTotal = 0
 
-print(total)
+for a, b in zip(listLeft, listRight):
+    distance = calculateDistances(a, b)
+    firstTotal += distance
 
+print(firstTotal)
+
+# part two
+secondTotal = 0
+
+listLeft = set(listLeft)
+
+counter = 0
+
+for a in listLeft:
+    for b in listRight:
+        if a == b:
+            counter += 1
+    secondTotal += a * counter
+    counter = 0
+
+print(secondTotal)
