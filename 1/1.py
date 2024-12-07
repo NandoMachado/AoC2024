f11855a1bdf53a5af0b3e8f2723ee575f8f0dde0
Nando Machado <icenando@gmail.com>
2024-12-07 14:46:41 +0000

    Day 1 - improved

--- 1/1.py
+++ 1/1.py
@@ -1,40 +1,55 @@
-def calculateDistances (a, b) -> int:
-    return abs(a-b)
+def calculateDistances(a, b) -> int:
+    return abs(a - b)
 
-with open("./input.txt", "r") as f:
-    lines = f.readlines()
 
-# To allocate the correct memory size from the outset
-listLeft, listRight = [0 for _ in range(len(lines))], [0 for _ in range(len(lines))]
+def readFile() -> list[str]:
+    with open("./input.txt", "r") as f:
+        return f.readlines()
 
-for index, item in enumerate(lines):
-    listLeft[index] = int(item.split()[0])
-    listRight[index] = int(item.split()[1])
 
-# part one
-listLeft.sort()
-listRight.sort()
+def createArrays(lines: list[str]) -> tuple[list[str], list[str]]:
+    # To allocate the correct memory size from the outset
+    listLeft, listRight = (
+        [None for _ in range(len(lines))],
+        [None for _ in range(len(lines))],
+    )
 
-firstTotal = 0
+    # Populate array with actual values
+    for index, item in enumerate(lines):
+        listLeft[index] = int(item.split()[0])
+        listRight[index] = int(item.split()[1])
 
-for a, b in zip(listLeft, listRight):
-    distance = calculateDistances(a, b)
-    firstTotal += distance
+    return (listLeft, listRight)
 
-print(firstTotal)
 
-# part two
-secondTotal = 0
+def partOne(lines: list[str]) -> None:
+    listLeft, listRight = createArrays(lines)
 
-listLeft = set(listLeft)
+    firstTotal = 0
 
-counter = 0
+    for _ in range(len(lines)):
+        leftMin = listLeft.pop(listLeft.index(min(listLeft)))
+        rightMin = listRight.pop(listRight.index(min(listRight)))
+        firstTotal += calculateDistances(leftMin, rightMin)
 
-for a in listLeft:
-    for b in listRight:
-        if a == b:
-            counter += 1
-    secondTotal += a * counter
-    counter = 0
+    print(firstTotal)
 
-print(secondTotal)
+
+def partTwo(lines: list[str]) -> None:
+    listLeft, listRight = createArrays(lines)
+    secondTotal = 0
+
+    listLeft = set(listLeft)
+
+    for a in listLeft:
+        counter = listRight.count(a)
+        secondTotal += a * counter
+        counter = 0
+
+    print(secondTotal)
+
+
+if __name__ == "__main__":
+    lines = readFile()
+    partOne(lines)
+    partTwo(lines)
